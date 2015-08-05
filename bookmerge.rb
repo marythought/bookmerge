35acf5cdf85070021053bd0d829ddfc20dc93c0c
Mary Dickson <mdickson@gmail.com>
2015-08-05 11:41:28 -0700

    added sinatra functionality and moved corpus to new ruby file

--- bookmerge.rb
+++ bookmerge.rb
@@ -1,5 +1,6 @@
 # optional, also allows you to specify version
 gem 'markov_chains'
+gem 'sinatra'
 
 # require 'mechanize'
 # mechanize = Mechanize.new
@@ -11,33 +12,37 @@ gem 'markov_chains'
 
 # just require and use it
 require 'markov_chains'
+require 'sinatra'
+require './corpus.rb'
 
-corpus = {
-  'The Adventures of Tom Sawyer' => 'tom_sawyer.txt',
-  'Alice in Wonderland' => 'alice_in_wonderland.txt',
-  'Anne of Green Gables' => 'anne_of_green_gables.txt',
-  'Beowulf' => 'beowulf.txt',
-  'The Bible' => 'the_bible.txt',
-  'The Brothers Grimm Fairy Tales' => 'grimms.txt',
-  'Jane Eyre' => 'jane_eyre.txt',
-  'Kama Sutra' => 'kama_sutra.txt',
-  'Little Women' => 'little_women.txt',
-  'Metamorphosis' => 'metamorphosis.txt',
-  'My Life, by Helen Keller' => 'helen_keller.txt',
-  'Peter Pan' => 'peter_pan.txt',
-  'A Picture of Dorian Gray' => 'dorian_gray.txt',
-  'Pride & Prejudice' => 'pride_and_prejudice.txt',
-  'Sherlock Holmes' => 'sherlock_holmes.txt',
-  '12 Years a Slave' => 'twelve_years_a_slave.txt',
-  'Ulysses' => 'ulysses.txt',
-  'War of the Worlds' => 'war_of_the_worlds.txt',
-  'The Wonderful Wizard of Oz' => 'wizard_of_oz.txt',
-  'The Yellow Wallpaper' => 'yellow_wallpaper.txt'
-}
+# corpus = {
+#   'The Adventures of Tom Sawyer' => 'tom_sawyer.txt',
+#   'Alice in Wonderland' => 'alice_in_wonderland.txt',
+#   'Anne of Green Gables' => 'anne_of_green_gables.txt',
+#   'Beowulf' => 'beowulf.txt',
+#   'The Bible' => 'the_bible.txt',
+#   'The Brothers Grimm Fairy Tales' => 'grimms.txt',
+#   'Jane Eyre' => 'jane_eyre.txt',
+#   'Kama Sutra' => 'kama_sutra.txt',
+#   'Little Women' => 'little_women.txt',
+#   'Metamorphosis' => 'metamorphosis.txt',
+#   'My Life, by Helen Keller' => 'helen_keller.txt',
+#   'Peter Pan' => 'peter_pan.txt',
+#   'A Picture of Dorian Gray' => 'dorian_gray.txt',
+#   'Pride & Prejudice' => 'pride_and_prejudice.txt',
+#   'Sherlock Holmes' => 'sherlock_holmes.txt',
+#   '12 Years a Slave' => 'twelve_years_a_slave.txt',
+#   'Ulysses' => 'ulysses.txt',
+#   'War of the Worlds' => 'war_of_the_worlds.txt',
+#   'The Wonderful Wizard of Oz' => 'wizard_of_oz.txt',
+#   'The Yellow Wallpaper' => 'yellow_wallpaper.txt'
+# }
+
+mycorpus = Corpus.new
 
 puts "Welcome to the book merger!"
 puts "Your book options are as follows:"
-puts corpus.keys
+puts mycorpus.keys
 puts "..."
 puts "Please enter your first text:"
 text1 = gets.chomp
@@ -46,9 +51,7 @@ text2 = gets.chomp
 puts "How many sentences? (max 100)"
 num_sentences = gets.chomp.to_i
 
-file1 = File.read('texts/' + corpus[text1])
-file2 = File.read('texts/' + corpus[text2])
-wholetext = file1 + file2
+wholetext = combine_texts(text1, text2, mycorpus)
 sentence_array = []
 
 # run the text through the markov generator to randomize it and save sentences in an array
@@ -66,16 +69,49 @@ sentence_array << generator.get_sentences(num_sentences)
 # puts sentence_array.class
 
 # let's make a shortcut for file name
-this_file = "markoved_#{corpus[text1]}_and_#{corpus[text2]}.txt"
+this_file = "markoved_#{text1.gsub(/\s+/, "")}_and_#{text2.gsub(/\s+/, "")}.txt"
 
 # save it as a text file, each sentence on a new line
 File.open(this_file, 'w+') do |file|
   file.puts(sentence_array)
 end
 
-puts "all done! you can view your file at 'markoved_#{corpus[text1]}_and_#{corpus[text2]}.txt'!"
-puts File.read(this_file) do
-  puts this_file
+puts "all done! you can view your file at '#{this_file}'!"
+
+displaytext = File.read(this_file)
+
+get '/' do
+  displaytext
 end
 
 
+
+
+# require 'rubygems'
+# require 'twitter'
+# require 'sinatra'
+
+# def twitter_id(screen_name)
+#   Twitter.user(screen_name).id
+# end
+
+# def is_following?(a,b)
+#   followers = Twitter.follower_ids(twitter_id(b)).ids
+#   followers.include?(twitter_id(a))
+# end
+
+# get '/' do
+
+#   erb :index
+# end
+
+# get '/follows' do
+#   @user1 = params[:user1]
+#   @user2 = params[:user2]
+
+#   @following = is_following?(@user1, @user2)
+
+#   erb :follows
+# end
+
+# from https://github.com/ashaw/ALA-Sample-Sinatra-App
---
+++ corpus.rb
@@ -0,0 +1,55 @@
+class Corpus
+  def initialize
+    @corpus = {
+    'The Adventures of Tom Sawyer' => 'tom_sawyer.txt',
+    'Alice in Wonderland' => 'alice_in_wonderland.txt',
+    'Anne of Green Gables' => 'anne_of_green_gables.txt',
+    'Beowulf' => 'beowulf.txt',
+    'The Bible' => 'the_bible.txt',
+    'The Brothers Grimm Fairy Tales' => 'grimms.txt',
+    'Jane Eyre' => 'jane_eyre.txt',
+    'Kama Sutra' => 'kama_sutra.txt',
+    'Little Women' => 'little_women.txt',
+    'Metamorphosis' => 'metamorphosis.txt',
+    'My Life, by Helen Keller' => 'helen_keller.txt',
+    'Peter Pan' => 'peter_pan.txt',
+    'A Picture of Dorian Gray' => 'dorian_gray.txt',
+    'Pride & Prejudice' => 'pride_and_prejudice.txt',
+    'Sherlock Holmes' => 'sherlock_holmes.txt',
+    '12 Years a Slave' => 'twelve_years_a_slave.txt',
+    'Ulysses' => 'ulysses.txt',
+    'War of the Worlds' => 'war_of_the_worlds.txt',
+    'The Wonderful Wizard of Oz' => 'wizard_of_oz.txt',
+    'The Yellow Wallpaper' => 'yellow_wallpaper.txt'
+    }
+  end
+
+  def add(books)
+    books.each do |title, file|
+      @corpus[title] = file
+    end
+  end
+
+  def keys
+    @corpus.keys
+  end
+
+  def lookup_file(title)
+    @corpus[title]
+  end
+
+end
+
+def combine_texts(text1, text2, library)
+  file1 = File.read('texts/' + library.lookup_file(text1))
+  file2 = File.read('texts/' + library.lookup_file(text2))
+  wholetext = file1 + file2
+end
+
+mycorpus = Corpus.new
+
+# # test combine text
+# puts combine_texts('Alice in Wonderland', 'The Yellow Wallpaper', mycorpus)
+
+# test lookup keys
+puts mycorpus.keys()
