d2fc6a8ab0792362fe6c343814413563f5cd1bc0
Mary Dickson <mdickson@gmail.com>
2015-08-05 14:17:46 -0700

    index page showing passed variables and merged text

--- bookmerge.rb
+++ bookmerge.rb
@@ -13,75 +13,49 @@ gem 'sinatra'
 # just require and use it
 require 'markov_chains'
 require 'sinatra'
+# moved booklist into a class in new file, combine_text function there also
 require './corpus.rb'
-
-# corpus = {
-#   'The Adventures of Tom Sawyer' => 'tom_sawyer.txt',
-#   'Alice in Wonderland' => 'alice_in_wonderland.txt',
-#   'Anne of Green Gables' => 'anne_of_green_gables.txt',
-#   'Beowulf' => 'beowulf.txt',
-#   'The Bible' => 'the_bible.txt',
-#   'The Brothers Grimm Fairy Tales' => 'grimms.txt',
-#   'Jane Eyre' => 'jane_eyre.txt',
-#   'Kama Sutra' => 'kama_sutra.txt',
-#   'Little Women' => 'little_women.txt',
-#   'Metamorphosis' => 'metamorphosis.txt',
-#   'My Life, by Helen Keller' => 'helen_keller.txt',
-#   'Peter Pan' => 'peter_pan.txt',
-#   'A Picture of Dorian Gray' => 'dorian_gray.txt',
-#   'Pride & Prejudice' => 'pride_and_prejudice.txt',
-#   'Sherlock Holmes' => 'sherlock_holmes.txt',
-#   '12 Years a Slave' => 'twelve_years_a_slave.txt',
-#   'Ulysses' => 'ulysses.txt',
-#   'War of the Worlds' => 'war_of_the_worlds.txt',
-#   'The Wonderful Wizard of Oz' => 'wizard_of_oz.txt',
-#   'The Yellow Wallpaper' => 'yellow_wallpaper.txt'
-# }
 
 mycorpus = Corpus.new
 
 puts "Welcome to the book merger!"
-puts "Your book options are as follows:"
+puts "You can choose two books from this list:"
 puts mycorpus.keys
 puts "..."
 puts "Please enter your first text:"
-text1 = gets.chomp
+# text1 = gets.chomp
+text1 = "The Bible"
 puts "Please enter your second text"
-text2 = gets.chomp
+# text2 = gets.chomp
+text2 = "Ulysses"
 puts "How many sentences? (max 100)"
-num_sentences = gets.chomp.to_i
+# num_sentences = gets.chomp.to_i
+num_sentences = 10
 
 wholetext = combine_texts(text1, text2, mycorpus)
-sentence_array = []
 
 # run the text through the markov generator to randomize it and save sentences in an array
-
 generator = MarkovChains::Generator.new(wholetext)
-sentence_array << generator.get_sentences(num_sentences)
+puts sentence_array = generator.get_sentences(num_sentences)
 
-# num_sentences.times do |i|
-#   i = markov.generate_1_sentence.strip
-#   sentence_array << i
+# let's not save a file anymore... getting annoying
+# # let's make a shortcut for file name
+# this_file = "markoved_#{text1.gsub(/\s+/, "")}_and_#{text2.gsub(/\s+/, "")}.txt"
+
+# # save it as a text file, each sentence on a new line
+# File.open(this_file, 'w+') do |file|
+#   file.puts(sentence_array)
 # end
 
-# test that markov script is working and creating an array
-# puts sentence_array
-# puts sentence_array.class
+# puts "all done! you can view your file at '#{this_file}'!"
 
-# let's make a shortcut for file name
-this_file = "markoved_#{text1.gsub(/\s+/, "")}_and_#{text2.gsub(/\s+/, "")}.txt"
-
-# save it as a text file, each sentence on a new line
-File.open(this_file, 'w+') do |file|
-  file.puts(sentence_array)
-end
-
-puts "all done! you can view your file at '#{this_file}'!"
-
-displaytext = File.read(this_file)
+# displaytext = File.read(this_file)
 
 get '/' do
-  displaytext
+  @displaytext = sentence_array
+  @corpus = mycorpus
+  erb :index
+
 end
 
 
